686d6c4de84b0a079bc9e4296c2e0d0097245b31
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-17 23:30:28 -0300

    feat(pages): create education view activity

---
+++ app/src/main/java/com/example/thebug_zoo/pages/EducationView.java
@@ -0,0 +1,16 @@
+package com.example.thebug_zoo.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.thebug_zoo.R;
+
+public class EducationView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education_view);
+    }
+}
