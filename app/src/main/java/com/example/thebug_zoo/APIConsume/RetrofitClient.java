fccbd56a27e956d8bfe2dec02c5cdaed8b5f6f9a
GuimaraesSl <aazzaazzaabb@gmail.com>
2022-03-04 00:47:24 -0300

    [LAST VERSION] PRIVACY POLICY DONE

--- app/src/main/java/com/example/thebug_zoo/APIConsume/RetrofitClient.java
+++ app/src/main/java/com/example/thebug_zoo/APIConsume/RetrofitClient.java
@@ -5,10 +5,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static String BASE_URL = "https://thebug-api.herokuapp.com/zoo/";
 
     public static Retrofit getRetrofitInstance(){
         if (retrofit == null){
+            String BASE_URL = "https://thebug-api.herokuapp.com/zoo/";
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
